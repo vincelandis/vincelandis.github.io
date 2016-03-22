af60ee103f76f679958c9f49d2363f1ea9ce8ff1
Vince Landis <vincelandis@gmail.com>
2016-03-21 20:21:44 -0400

    added tab bar, search bar, construction notice

---
+++ scripts/vincelandis.js
@@ -0,0 +1,11 @@
+/*
+
+document.addEventListener("DOMContentLoaded", function() {
+  
+  for (var i = 1; i <= buttonCount; i++) {
+    document.getElementById("button" + i).addEventListener("click", chooseMenuTab, false);
+  }
+
+}, false);
+
+*/
