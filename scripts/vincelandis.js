b530afac2e0a5060092d92ea9e2cbb8fc9760807
Vince Landis <vincelandis@gmail.com>
2016-04-06 00:30:39 -0400

    added contact links, new construction notice

--- scripts/vincelandis.js
+++ scripts/vincelandis.js
@@ -1,11 +1,40 @@
+var warnings;
+var dismissers;
+
 document.addEventListener("DOMContentLoaded", function() {
   
-   document.getElementById("searchBar").addEventListener("keyup", searchSite, false);
+   document.getElementById("searchBar").addEventListener("keyup", respondKeyInput, false);
+   document.getElementById("magnifying-glass").addEventListener("click", searchSite, false);
+   
+   warnings = document.getElementsByClassName("warning");
+   dismissers = document.getElementsByClassName("dismiss_cross");
 
+   for (var i = 0; i < warnings.length; i++) {
+      dismissers[i].addEventListener("click", dismissWarning, false);
+   }
+   
+   window.onresize = resizeResults;
+   resizeResults();
+   
 }, false);
 
-function searchSite(e) {
-   if (e.keyCode == "13") {
-      document.getElementById("searchBar").value = "Nahh";
+function respondKeyInput(e) {
+   if (e.keyCode == "13")
+      searchSite();
+}
+
+function searchSite() {
+   document.getElementById("searchBar").value = "Nahh";
+}
+
+function resizeResults() {
+  document.getElementById("displayArea").style.width = window.innerWidth - 200;
+}
+
+function dismissWarning(e) {
+   
+   for (var i = 0; i < dismissers.length; i++) {
+      if (e.target = dismissers[i])
+         warnings[i].style.display = "none";
    }
 }
